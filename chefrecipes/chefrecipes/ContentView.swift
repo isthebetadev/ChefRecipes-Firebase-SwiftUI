90937680a9d5a1223641cd7f40bbd9ede50d325d
Rubén <ruben.sr.apple@gmail.com>
2024-06-05 18:26:07 +0200

    Create xcode project

---
+++ chefrecipes/chefrecipes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chefrecipes
+//
+//  Created by Ruben on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "fork.knife")
+                .tint(.black)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .padding(.bottom, 4)
+            Text("Hello, Chef Recipes! 👨🏻‍🍳")
+                .bold()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chefrecipes/chefrecipes/chefrecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  chefrecipesApp.swift
+//  chefrecipes
+//
+//  Created by Ruben on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct chefrecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
